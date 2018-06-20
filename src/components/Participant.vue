968b88098f4bfc6a767d33d93411e7a2de11dca0
Jani Eriksson <jani.eriksson@3stepit.com>
2018-06-20 14:20:11 +0300

    single participant page skeleton added

---
+++ src/components/Participant.vue
@@ -0,0 +1,12 @@
+<template>
+    <h4>participant: </h4>
+</template>
+
+<script>
+export default {
+  name: 'participant',
+};
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Dashboard from '@/components/Dashboard';
+import Participant from '@/components/Participant';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'landing',
       component: Dashboard,
+    },
+    {
+      path: '/participants/:participantId',
+      name: 'participant',
+      component: Participant,
     },
   ],
 });
